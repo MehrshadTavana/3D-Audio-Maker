737c075adc9e0b0d1dd0fdd933d958671b329b15
Mehrshad Tavana <102242930+MehrshadTavana@users.noreply.github.com>
2025-06-16 19:48:34 -0600

    Update Main.m

--- Main.m
+++ Main.m
@@ -10,9 +10,7 @@ PathGeneration
 load('./Recordings/Impulse_Responses.mat');
 
 
-%%%%%%%%%%%%%
 % generating stereo output
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % defining the output audio samples for the left and right ears
 Output_Audio_Left       = zeros(length(Input_Audio_samples) + far.ImpResp_Length , 1);
 Output_Audio_Right      = zeros(length(Input_Audio_samples) + far.ImpResp_Length , 1);
